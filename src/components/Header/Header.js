2e6b8ab511517ce8c9776b43dd14cb9cb27c52c0
Владислав Исып <vladislavisyp@MacBook-Pro-Vladislav.local>
2022-11-21 19:41:49 +0200

    header_structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,9 @@ import "./header.css"
 const Header = () => {
   return (
       <header>
-      12ajajnwjawdakmw
+      <div className="logo"></div>
+      <div className="buttons"></div>
+      <div className="search"></div>
       </header>
   )
 }
